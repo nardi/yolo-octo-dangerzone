93114c8787e709bd2b883c3c08babf8e2b27c470
Bas Verhaar <bverhaar@live.nl>
2013-06-21 16:49:32 +0200

    pull

---
+++ GameTest/src/com/example/gametest/Character.java
@@ -0,0 +1,19 @@
+package com.example.gametest;
+
+import android.graphics.Canvas;
+
+public class Character implements GameObject {
+
+	@Override
+	public void onDraw(Canvas canvas) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onUpdate(long dt) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
