4d941fe0c5b84053005c42625a0eb7df9d861e4b
ThuisPc <dimitri.belfor@gmail.com>
2013-06-23 02:00:19 +0200

    nu moet hij kloppen

--- GameTest/src/com/example/gametest/SimpleBeatDetector.java
+++ GameTest/src/com/example/gametest/SimpleBeatDetector.java
@@ -7,35 +7,62 @@ package com.example.gametest;
  * Dit kan trouwens ook een interface worden, maken we een SimpleBeatDetector en
  * later een FFTBeatDetector
  */
-public class SimpleBeatDetector implements IBeatDetector {
-
+public class SimpleBeatDetector implements BeatDetector {
+	
+	private CircularDoubleBuffer historyBuffer;
+	private double[] tempBuffer;
+	int writeEnd = 0;
+	double instantEnergy = 0;
+	double localEnergy = 0;
+	
 	// reference is om voor de eerste energies te kunnen bepalen of het beats
 	// zijn, i.e. reference is historySize groot
-	public SimpleBeatDetector(int historySize, int beatFactor, double[] reference) {
-		...
-		historyBuffer.placeFrom(0, reference, 0, historySize);
+	public SimpleBeatDetector (int historySize, int beatFactor, double[] reference) {
+		tempBuffer = new double[historySize];//Gebruikt voor berekeningen op history
+		historyBuffer = new CircularDoubleBuffer(historySize);
+		writeEnd = historyBuffer.placeFrom(0, reference, 0, historySize);
 	}
 	
-	public boolean newSamples(double[] samples) {
-		...
+	public boolean newEnergy(double energy) {
+		boolean isBeat = false;
+		historyBuffer.getFrom(0, tempBuffer, 0, tempBuffer.length);
+		double c = calcC(calcVariance(tempBuffer));
+		if (instantEnergy > (c * localEnergy)) {
+			isBeat = true;
+		}
 		return isBeat;
 	}
 	
-	public double setEnergy (double[] samples) {
-		double localEnergy = 0;
+	public boolean newSamples (double[] samples) {
+		double instantEnergy = 0;
 		for (int i = 0; i < samples.length; i++) {
-			localEnergy += (samples[i] * samples[i]);
+			instantEnergy += (samples[i] * samples[i]);
 		}
-		return (localEnergy / samples.length);
+		this.instantEnergy = (instantEnergy * 1024) /samples.length;
+	}
+	
+	private void updateHistory () {
+		writeEnd = historyBuffer.setSingle(writeEnd, instantEnergy);
 	}
 	
 	// Berekent de afwijking van de locale energy met zijn history
-	private double calcVariance (double[] energyHistory, double localEnergy) {
+	private double calcVariance (double[] energyHistory) {
 		double variance = 0;
+		double localEnergy = 0;
+		
+		// Krijg local energy total
+		for (int i = 0; i < energyHistory.length; i++) {
+			localEnergy += (energyHistory[i] * energyHistory[i]);
+		}
+		
+		localEnergy /= energyHistory.length;
+		this.localEnergy = localEnergy;
+		// Krijg variantie total
 		for (int i = 0; i < energyHistory.length; i++) {
 			variance += ((energyHistory[i] - localEnergy) * (energyHistory[i] - localEnergy));
 		}
-		return (variance/energyHistory.length);
+		
+		return (variance /energyHistory.length);
 	}
 	
 	// Berekent de constante C
@@ -52,7 +79,7 @@ public class SimpleBeatDetector implements IBeatDetector {
 		
 	}
 	
-	public Section[] getSections() {
-		
+	public Section[] getSections () {
+
 	}
 }
