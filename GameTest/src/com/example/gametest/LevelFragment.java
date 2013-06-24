49a49746ae2d036209de2dc635f17a88be6bedf2
MetalPinguin <gitpinguin@gmail.com>
2013-06-24 13:59:44 +0200

    digen

--- GameTest/src/com/example/gametest/LevelFragment.java
+++ GameTest/src/com/example/gametest/LevelFragment.java
@@ -50,20 +50,18 @@ public class LevelFragment extends GameFragment {
 	
 	public FloorPoint[] generateDevs(){
 		FloorPoint[] array = new FloorPoint[400];
-		for (int i = 0; i < 40; i++) {
+		/*for (int i = 0; i < 40; i++) {
 			array[i] = new FloorPoint(0.0);
 		}
 		for (int i = 0; i < 360; i++) {
 			array[i+40] = new FloorPoint(Math.sin(Math.toRadians(i)));
-		}
+		}*/
 		
-		/*
-		FloorPoint[] array = new FloorPoint[400];
 		Random random = new Random();
 		random.setSeed(42);
 		for(int i = 0; i < 400; i++){
 			array[i] = new FloorPoint(random.nextFloat());
-		} */
+		}
 		return array;
 	}
 }
