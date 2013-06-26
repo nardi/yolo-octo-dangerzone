517f26fd421dec7f5f52a93f8a76843fe6ef7879
Dimitri Belfor <dimitri.belfor@gmail.com>
2013-06-26 18:09:23 +0200

    beetje menu

--- GameTest/src/yolo/octo/dangerzone/lvlgen/LevelDraw.java
+++ GameTest/src/yolo/octo/dangerzone/lvlgen/LevelDraw.java
@@ -119,7 +119,7 @@ public class LevelDraw {
 
 		}
 		Paint paint2 = new Paint();
-		paint2.setColor(Color.BLUE);
+		paint2.setColor(Color.rgb(183, 219, 149));
 		path.lineTo(view.getWidth(), view.getHeight());
 		path.lineTo(0, view.getHeight());
 		canvas.drawPath(path, paint2);
