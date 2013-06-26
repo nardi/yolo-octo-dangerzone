eddf917039bc4a65771bb0d7364c26dbe7bb8f18
MetalPinguin <gitpinguin@gmail.com>
2013-06-26 13:55:50 +0200

    Bound gefixed

--- GameTest/src/yolo/octo/dangerzone/lvlgen/LevelGenerator.java
+++ GameTest/src/yolo/octo/dangerzone/lvlgen/LevelGenerator.java
@@ -104,7 +104,7 @@ public class LevelGenerator {
 				/*
 				 * Finding the end of the gap.
 				 */
-				while(level[q] == -2){
+				while(level[q] == -2 && q < level.length){
 					q++;
 				}
 				/*
